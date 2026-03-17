a5e19118455588112e7071db92d12b57d87ae032
Daniel Lopez <daniellop199731@gmail.com>
2026-03-17 17:05:03 -0500

    Modificando header del request
    -Al ejecutar exchange.mutate(), se crea una copia del exchange original, el nuevo header se aplica a la copia mas no al original, por este motivo desde la linea 27 se sigue usando mutatedExchange y no exchange

--- src/main/java/com/daniel/springcloud/app/gateway/infrastructure/filters/SampleGlobalFilter.java
+++ src/main/java/com/daniel/springcloud/app/gateway/infrastructure/filters/SampleGlobalFilter.java
@@ -1,9 +1,12 @@
 package com.daniel.springcloud.app.gateway.infrastructure.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,22 +15,38 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter{
+public class SampleGlobalFilter implements GlobalFilter, Ordered {
 
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
-
+    
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        
-        logger.info("Ejecutando el filtro antes del request PRE");
 
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            logger.info("Ejecutando el filtro POST response");
+        logger.info("ejecutando el filtro antes del request PRE");
 
-            exchange.getResponse().getCookies().add("Color", ResponseCookie.from("Color", "red").build());
-            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+        ServerWebExchange mutatedExchange = exchange.mutate()
+            .request(r -> r.headers(h -> h.add("token", "abcdefg")))
+            .build();
+
+        logger.info("token PRE: " + mutatedExchange.getRequest().getHeaders().getFirst("token"));            
+
+        return chain.filter(mutatedExchange).then(Mono.fromRunnable(() -> {
+            logger.info("ejecutando filtro POST response");
+
+            Optional.ofNullable(mutatedExchange.getRequest().getHeaders().getFirst("token"))
+            .ifPresent(value -> {
+                logger.info("token2: " + value);
+                mutatedExchange.getResponse().getHeaders().add("token2", value);
+            });
+
+            mutatedExchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
+            mutatedExchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
         }));
-        
     }
     
+    @Override
+    public int getOrder() {
+        return 100;
+    }
+
 }
